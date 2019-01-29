77ea14874ea05d9c016b449a3f59c448488e617c
Sebastian <sebastian.goenaga@hotmail.com>
2019-01-29 10:59:53 -0500

    Funcionando

--- src/main/java/edu/escuelaing/arem/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arem/SparkWebApp.java
@@ -6,8 +6,65 @@ public class SparkWebApp {
 
     public static void main(String[] args) {
         port(getPort());
-        get("/hello", (req, res) -> "Hello Heroku");
-        
+        get("/index", (req, res) -> "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<body>\n"
+                + "\n"
+                + "<h2>Calculadora de medias y desviaciones estandar</h2>\n"
+                + "\n"
+                + "<form action=\"/resp\">\n"
+                + "  Lista de números<br>\n"
+                + "  <input type=\"text\" name=\"numeros\" value=\"\">\n"
+                + "  <br><br>\n"
+                + "  <input type=\"submit\" value=\"Submit\">\n"
+                + "</form> \n"
+                + "\n"
+                + "\n"
+                + "</body>\n"
+                + "</html>");
+        get("/resp", (req, res) -> "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<body>\n"
+                + "\n"
+                + "<h2>Respuesta</h2>\n"
+                + "\n"
+                + "<form action=\"/index\">\n"
+                + "  Media:<br>\n"
+                + media(req.queryParams("numeros"))
+                + "  <br>\n"
+                + "  <br> Desviacion: <br>\n"
+                + desv(req.queryParams("numeros"))
+                + "  <br><br>\n"
+                + "  <input type=\"submit\" value=\"Volver\">\n"
+                + "</form> \n"
+                + "\n"
+                + "\n"
+                + "</body>\n"
+                + "</html>"
+                + "\n");
+
+    }
+
+    static float media(String numeros) {
+        float suma = 0;
+        String[] num = numeros.split(" ");
+        int n = num.length;
+        for (int i = 0; i < n; i++) {
+            suma += Float.parseFloat(num[i]);
+        }
+        return suma / n;
+    }
+
+    static float desv(String numeros) {
+        float suma = 0;
+        String[] num = numeros.split(" ");
+        int n = num.length;
+        float media = media(numeros);
+        for (int i = 0; i < n; i++) {
+            suma += Math.pow(Float.parseFloat(num[i]) - media, 2);
+        }
+
+        return (float) Math.sqrt(suma / (n - 1));
     }
 
     static int getPort() {
